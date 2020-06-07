c4d59093d135a3c64a9a3f164d4bb75b8a5e4d61
Mansi <52520911+pyprogr@users.noreply.github.com>
2020-06-07 11:47:14 +0530

    Update import.py

--- import.py
+++ import.py
@@ -9,13 +9,14 @@ db = scoped_session(sessionmaker(bind=engine))
 
 def main():
     # Create table to import data into
-    #TABLE TO STORE USER'S INFORMATION
-    db.execute("CREATE TABLE users (user_id SERIAL PRIMARY KEY, firstname VARCHAR NOT NULL, lastname VARCHAR NOT NULL, username VARCHAR NOT NULL, password VARCHAR NOT NULL)")
+    #TABLE TO STORE USER INFORMATION
+
+    db.execute("CREATE TABLE users(user_id SERIAL PRIMARY KEY, firstname VARCHAR NOT NULL, lastname VARCHAR NOT NULL, username VARCHAR NOT NULL, password VARCHAR NOT NULL)")
     #TABLE TO STORE POLLS CREATED
     db.execute("CREATE TABLE poll (question VARCHAR NOT NULL, option1 VARCHAR NOT NULL,option2 VARCHAR NOT NULL,
                 option3 VARCHAR ,option4 VARCHAR ,pollid VARCHAR NOT NULL PRIMARY KEY,
                 user_id INTEGER NOT NULL FOREIGN KEY REFERENCES users(user_id),ended INTEGER DEFAULT 0)")
-    #TABLE TO STORE VOTES' INFORMATION
+    #TABLE TO STORE VOTES INFORMATION
     db.execute("CREATE TABLE votes (pollid INTEGER NOT NULL FOREIGN KEY REFERENCES poll(pollid),
                 user_id INTEGER NOT NULL FOREIGN KEY REFERENCES user(user_id),
                 option INTEGER NOT NULL)")
