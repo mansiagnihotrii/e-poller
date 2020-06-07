6e6620c8b07ad4395e97158555a5d685bfcdfc03
Mansi <52520911+pyprogr@users.noreply.github.com>
2020-06-07 18:38:52 +0530

    Bugs fixed

--- import.py
+++ import.py
@@ -10,17 +10,13 @@ db = scoped_session(sessionmaker(bind=engine))
 def main():
     # Create table to import data into
     #TABLE TO STORE USER INFORMATION
-
-    db.execute("CREATE TABLE users(user_id SERIAL PRIMARY KEY, firstname VARCHAR NOT NULL, lastname VARCHAR NOT NULL, username VARCHAR NOT NULL, password VARCHAR NOT NULL)")
+    db.execute("CREATE TABLE users (user_id SERIAL PRIMARY KEY, firstname VARCHAR NOT NULL, lastname VARCHAR NOT NULL, username VARCHAR NOT NULL, password VARCHAR NOT NULL)")
     #TABLE TO STORE POLLS CREATED
-    db.execute("CREATE TABLE poll (question VARCHAR NOT NULL, option1 VARCHAR NOT NULL,option2 VARCHAR NOT NULL,
-                option3 VARCHAR ,option4 VARCHAR ,pollid VARCHAR NOT NULL PRIMARY KEY,
-                user_id INTEGER NOT NULL FOREIGN KEY REFERENCES users(user_id),ended INTEGER DEFAULT 0)")
+    db.execute("CREATE TABLE poll (question VARCHAR NOT NULL, option1 VARCHAR NOT NULL,option2 VARCHAR NOT NULL,option3 VARCHAR ,option4 VARCHAR ,pollid INTEGER NOT NULL PRIMARY KEY,user_id INTEGER NOT NULL REFERENCES users(user_id))")
     #TABLE TO STORE VOTES INFORMATION
-    db.execute("CREATE TABLE votes (pollid INTEGER NOT NULL FOREIGN KEY REFERENCES poll(pollid),
-                user_id INTEGER NOT NULL FOREIGN KEY REFERENCES user(user_id),
-                option INTEGER NOT NULL)")
-
+    db.execute("CREATE TABLE votes (pollid INTEGER NOT NULL  REFERENCES poll(pollid),user_id INTEGER NOT NULL  REFERENCES users(user_id),option VARCHAR NOT NULL)")
+    db.commit()
 
 if __name__ == "__main__":
     main()
+
